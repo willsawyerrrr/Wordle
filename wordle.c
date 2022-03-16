35a52031b8aa2124e884a4acb0b283b1a5b043cc
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-03-16 15:11:52 +0000

    add command line argument handling
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a1@7 a1968c47-03bd-fd43-9cb2-c192510db353

--- wordle.c
+++ wordle.c
@@ -1,6 +1,42 @@
 #include <csse2310a1.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-    puts("Usage: wordle [-len word-length] [-max max-guesses] [dictionary]");
-    return 1;
+    int valid = 1;
+    int wordLength = 5;
+    int wordSet = 0;
+    int maxGuesses = 6;
+    int guessesSet = 0;
+    char* dictionaryName = "";
+    
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-len") == 0) {
+            if (i < argc - 1 && !wordSet) {
+                wordLength = atoi(argv[i + 1]);
+                wordSet = 1;
+            } else {
+                valid = 0;
+            } 
+        } else if (strcmp(argv[i], "-max") == 0) {
+            if (i < argc -1 && !guessesSet) {
+                maxGuesses = atoi(argv[i + 1]);
+                guessesSet = 1;
+	        } else {
+    	    	valid = 0;
+	        }
+        } else if (((int) argv[i][0]) == '-') {
+            valid = 0;
+        } else if (atoi(argv[i]) == 0) {
+            dictionaryName = argv[i];
+        }
+    }
+
+    if (!valid) {
+        puts("Usage: wordle [-len word-length] [-max max-guesses] [dictionary]");
+        return 1;
+    }
+
+    return 0;
 }
