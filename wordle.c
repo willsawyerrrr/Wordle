e5ba9d3edea699d203947ea849923b4f9e66e436
William Sawyer <w.sawyer@uqconnect.edu.au>
2022-03-23 04:13:33 +0000

    fix invalid guesses incrementing guess count
    git-svn-id: https://source.eait.uq.edu.au/svn/csse2310-sem1-s4696360/trunk/a1@36 a1968c47-03bd-fd43-9cb2-c192510db353

--- wordle.c
+++ wordle.c
@@ -99,6 +99,7 @@ void play_game(int wordLength, int maxGuesses, char dictionaryPath[]) {
             printf("%s\n", report_matches(guess, answer));
         } else {
             printf("Word not found in the dictionary - try again.\n");
+            current--;
         } 
     }
 
